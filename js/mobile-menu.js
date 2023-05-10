c6c0d5937830de1f9130c809300850ef2861155a
kruhitka1706 <kruhitka1706@gmail.com>
2023-05-10 22:18:23 +0300

    Create mobile-menu.js

---
+++ js/mobile-menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const menuContainer = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => menuContainer.classList.toggle('is-open');
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  
+  window
+  .matchMedia('(min-width: 600px)')
+  .addEventListener(
+    'change',
+    e => e.matches && menuContainer.classList.remove('is-open'),
+  );
+})();
